4fb086be3683cc76976222c4fdc749ec85481a21
jerry <jerry@LAPTOP-98P3UM8O>
2022-12-27 10:02:02 +0800

    first commit

---
+++ src/main/java/com/example/userInfo_project/UserInfoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.userInfo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInfoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserInfoProjectApplication.class, args);
+	}
+
+}
